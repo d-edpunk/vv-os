2187cb636b91b97e8292992c77d323fa99a1b828
ip_4ort <russian.hacker.shrug@googlemail.com>
2022-11-09 19:12:04 +0300

    3.1γ

--- android/app/src/main/kotlin/com/example/vv_os/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vv_os
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'src/home.dart';
-
-void main() => runApp(const MaterialApp(home: HomePage()));
